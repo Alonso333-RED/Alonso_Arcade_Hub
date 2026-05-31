0aeea416a8b1c41607f1c07b182671a092d17e36
Alonso333-RED <alonsitoar018@gmail.com>
2026-05-31 03:46:50 -0400

    First Upload

---
+++ alonso_arcade_hub/games_listing_engine/game_list.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name GameList
+
+@export var games: Array[GameMetadata]
---
+++ alonso_arcade_hub/games_listing_engine/game_metadata.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name GameMetadata
+
+@export var name: String
+@export_multiline var description: String
+@export var preview: String
+@export var scene: String
