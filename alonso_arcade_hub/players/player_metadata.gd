15f9eb53f85ad52bd16761708c740e6fe720f2f7
Alonso333-RED <alonsitoar018@gmail.com>
2026-06-16 05:02:17 -0400

    Working on a new game called Tankecito

---
+++ alonso_arcade_hub/players/player_metadata.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name PlayerMetadata
+
+@export var name: String
+@export var color: Color
+@export var prefix: String
--- alonso_arcade_hub/utils/back.gd
+++ alonso_arcade_hub/utils/back.gd
@@ -1,5 +1,7 @@
 extends Button
 
+@export var ghost_mode := false
+
 var holding := false
 var hold_time := 0.0
 var required_time := 1.0
@@ -8,6 +10,9 @@ var activated := false
 func _ready():
 	button_down.connect(_on_button_down)
 	button_up.connect(_on_button_up)
+
+	if ghost_mode:
+		_make_hidden()
 
 func _process(delta):
 	if holding and not activated:
@@ -22,10 +27,22 @@ func _on_button_down():
 	hold_time = 0.0
 	activated = false
 
+	if ghost_mode:
+		_make_visible()
+
 func _on_button_up():
 	holding = false
 	hold_time = 0.0
 	activated = false
 
+	if ghost_mode:
+		_make_hidden()
+
 func activate():
 	get_tree().change_scene_to_file("res://MainMenu/MainMenu.tscn")
+
+func _make_hidden():
+	modulate.a = 0.0
+
+func _make_visible():
+	modulate.a = 1.0
